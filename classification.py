0587c17e14203f39db99292018f1d5d17e0e699c
BrunoSilva <119140240+Avlis03@users.noreply.github.com>
2024-01-26 12:44:59 +0000

    version 1

---
+++ classification.py
@@ -0,0 +1,133 @@
+from keras.applications import VGG16, InceptionV3, ResNet50V2, InceptionResNetV2, Xception, VGG19
+from keras.preprocessing.image import ImageDataGenerator
+from keras import models
+from keras import layers
+from keras import optimizers
+import matplotlib.pyplot as plt
+import keras
+from keras.models import load_model
+import os
+import tensorflow as tf
+from keras import backend as K
+import dill
+import focal_loss
+
+
+
+##FOLDERS
+train_dir= './Dataset_patches/wra/train'
+val_dir= './Dataset_patches/wra/validation'
+test_dir= './Dataset_patches/wra/test'
+
+
+#MODEL
+conv=model(weights='imagenet', include_top=False, input_shape= (256,256,3))
+
+model = conv.output
+model = layers.Flatten()(model)
+model = layers.Dense(1024, activation='relu')(model)
+model = layers.BatchNormalization()(model)
+model = layers.Dense(512, activation='relu')(model)
+model = layers.BatchNormalization()(model)
+model = layers.Dense(256, activation='relu')(model)
+model = layers.BatchNormalization()(model)
+model = layers.Dense(128, activation='relu')(model)
+model = layers.BatchNormalization()(model)
+model = layers.Dense(64, activation='relu')(model)
+model = layers.BatchNormalization()(model)
+model = layers.Dense(32, activation='relu')(model)
+model = layers.BatchNormalization()(model)
+out = layers.Dense(1, activation='sigmoid')(model)
+
+model = models.Model(inputs=conv.input, outputs=out)
+
+
+conv.trainable = True
+set_trainable = False
+
+for layer in model.layers:
+    if layer.name == 'block5_conv1':
+        set_trainable = True
+    if set_trainable:
+        layer.trainable = True
+    else:
+        layer.trainable = False
+
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255)    
+
+
+#OPEN AND PREPARE DATASET
+datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(  
+    train_dir,
+    target_size=(256,256),
+    batch_size=20,
+    class_mode='binary')
+
+validation_generator = datagen.flow_from_directory(  
+    val_dir,
+    target_size=(256,256),
+    batch_size=20,
+    class_mode='binary')
+
+
+#CALLBACK SAVE BEST EPOCH
+callb= [keras.callbacks.ModelCheckpoint(filepath='./FL2_vgg19_wra.h5', 
+                                        monitor='val_loss', 
+                                        save_best_only=True)]
+
+
+#TRAIN MODEL
+model.compile(loss=focal_loss.binary_focal_loss(gamma=2., alpha=.25),
+              optimizer=optimizers.Adam(lr=0.00001),
+              metrics=['accuracy']
+)
+
+history = model.fit(
+    train_generator,
+    steps_per_epoch=260,
+    epochs=150,
+    validation_data=validation_generator,
+    validation_steps=60,
+    callbacks=callb)
+
+
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(21, len(acc) + 21) #change to 1
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
+
+dict_h=history.history
+
+###########################
+#EVALUATION
+############################
+test_generator = datagen.flow_from_directory(
+    test_dir,
+    target_size=(256,256),
+    batch_size=20,
+    class_mode='binary')
+
+
+model=load_model('./vgg19.h5', 
+                 custom_objects={'binary_focal_loss_fixed': focal_loss.binary_focal_loss()} 
+                 )
+
+teste= model.evaluate(test_generator)
+
+
---
+++ segmentation.py
@@ -0,0 +1,112 @@
+import tensorflow as tf
+import keras
+#%env SM_FRAMEWORK=tf.keras
+import segmentation_models as sm
+from keras.models import load_model
+from keras import optimizers
+from keras.preprocessing.image import img_to_array, load_img, array_to_img
+import glob
+import cv2
+import os 
+import numpy as np 
+import matplotlib.pyplot as plt
+import random
+from keras import backend as K
+import dill
+import random
+from keras.preprocessing.image import ImageDataGenerator
+import segmentation_metrics
+
+
+#FOLDERS
+train_folder='./Dataset_patches/wra/train/earthquake'
+validation_folder='./Dataset_patches/wra/validation/earthquake'
+test_folder='./Dataset_patches/wra/test/earthquake'
+
+train_mask_folder='./Dataset_patches/wra/masks_train'
+validation_mask_folder='./Dataset_patches/wra/masks_validation'
+
+
+
+def generator_img(folder):
+    for i in os.listdir(folder):
+        img_= os.path.join(folder, i)
+        img_= load_img(img_)
+        img_= img_to_array(img_)
+        img_[:] /= 255
+        yield(img_)
+    
+def generator_mask(folder):
+    for i in os.listdir(folder):
+        mask_= os.path.join(folder, i)
+        mask_= cv2.imread(mask_, 0).astype(np.float32)  
+        mask_[:]= mask_/255
+        mask_=np.expand_dims(mask_, axis=2)
+        yield(mask_)
+
+def my_image_mask_generator(generator_img, generator_mask, batch_size):
+        gen = list(zip(generator_img, generator_mask))
+        imgs=[]
+        masks=[]
+        count=0
+        while True:                
+                for (img, mask) in gen:
+                    imgs.append(img)
+                    masks.append(mask)
+                    if len(imgs)==batch_size:
+                        imgs=np.asarray(imgs)
+                        masks=np.asarray(masks)
+                        
+                        yield (imgs, masks)   
+                       
+                        imgs=[]
+                        masks=[]
+                    
+                    else:
+                        continue
+
+
+#OPEN  TRAIN GENERATORS
+images_folder=train_folder
+masks_folder=train_mask_folder
+
+train_img_generator=generator_img(images_folder)
+train_mask_generator= generator_mask(masks_folder)
+
+train_generator= my_image_mask_generator(train_img_generator, train_mask_generator, batch_size=16)
+
+
+#OPEN  VALIDATION GENERATORS
+images_val_folder=validation_folder
+masks_val_folder=validation_mask_folder
+
+val_img_generator=generator_img(images_val_folder)
+val_mask_generator= generator_mask(masks_val_folder)
+
+val_generator= my_image_mask_generator(val_img_generator, val_mask_generator, batch_size=16)
+
+
+model= sm.Unet('vgg19',
+               classes=1,
+               encoder_weights='imagenet')
+
+model.compile(optimizer=optimizers.Adam(lr=0.0001), 
+              loss=sm.losses.bce_jaccard_loss, 
+              metrics=[segmentation_metrics.dice_coef, segmentation_metrics.mean_iou,'acc'] )  
+
+
+callb= [keras.callbacks.ModelCheckpoint(filepath='./seg_vgg19_wra.h5', 
+                                        monitor='val_loss', 
+                                        save_best_only=True),
+       keras.callbacks.ReduceLROnPlateau(monitor='val_dice_coef', factor=0.95,
+                                        patience=3, verbose=1, mode='max',
+                                         cooldown=1, min_lr=0.00001)]
+
+
+history = model.fit(train_generator,
+                    steps_per_epoch=125,
+                    epochs=150,
+                    validation_data=val_generator,
+                    validation_steps=62,
+                    callbacks=callb
+                    )
